5ce40dc13340734ffff3a76c54ff53e5421eba2d
Jean Cheng <jxc3889@rit.edu>
2015-03-21 13:03:43 -0400

    invoice class

---
+++ Project/src/Invoice.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 21, 2015
+
+@author: Jean
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
